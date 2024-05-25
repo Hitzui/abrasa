f0ad9b3a9255e1f4cb7f968f93c3a83c2b9de080
jose miguel romero sevilla <jmiguelsevilla@hotmail.com>
2024-05-24 20:31:53 -0600

    update 17 v2

--- views/site/index.php
+++ views/site/index.php
@@ -19,9 +19,9 @@ $this->title = 'Agropecuaria Bravo, S.A. - ABRASA';
 /** @var Slide $slide */
 $css = <<<CSS
 .video-container {
-            position: relative;
-            padding-bottom: 56.25%; /* 16:9 ratio */
-            height: 100%;
+            /*position: relative;
+            padding-bottom: 56.25%;*/ /* 16:9 ratio */
+            height: 750px;
             overflow: hidden;
             max-width: 100%;
             background: #000;
@@ -32,7 +32,7 @@ $css = <<<CSS
             top: 0;
             left: 0;
             width: 100%;
-            height: 68%;
+            height: 100%;
             border: 0;
         }
 .efecto img {
