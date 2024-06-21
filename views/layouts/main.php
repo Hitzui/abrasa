acd8c48c10e2d85515ce9cb3561fc0f7321f6134
jose miguel romero sevilla <jmiguelsevilla@hotmail.com>
2024-06-21 08:19:40 -0600

    update 45

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,7 +39,6 @@ $categorias = Categoria::find()->orderBy('nombre')->all();
     <title><?= Html::encode($this->title) ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <link rel="shortcut icon" href="/assets/img/favicon.ico" type="image/x-icon"/>
-    <link rel="apple-touch-icon" href="/assets/img/apple-touch-icon.png"/>
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link href="https://fonts.googleapis.com/css2?family=Lora:wght@500&display=swap" rel="stylesheet"/>
