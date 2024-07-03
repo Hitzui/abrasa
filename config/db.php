c91f1a534029bca8f3a33cc70f8d85350d32d9c5
jose miguel romero sevilla <jmiguelsevilla@hotmail.com>
2024-07-03 11:38:33 -0600

    update 94

--- config/db.php
+++ config/db.php
@@ -3,11 +3,11 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost:3306;dbname=NIN-2699402_ABRASA',
-/*    'username' => 'adminabrasa',
-    'password' => '2ugU465y!',*/
+    'username' => 'adminabrasa',
+    'password' => '2ugU465y!',
     'charset' => 'utf8',
-    'username' => 'root',
-    'password' => 'admin',
+/*    'username' => 'root',
+    'password' => 'admin',*/
 
     // Schema cache options (for production environment)
     'enableSchemaCache' => true,
