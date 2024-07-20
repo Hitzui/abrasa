52c78a3a26415bc8e890f8d934ac0ce2f901f59d
jose miguel romero sevilla <jmiguelsevilla@hotmail.com>
2024-07-20 07:02:01 -0600

    update 117

--- views/detaarticulo/_form.php
+++ views/detaarticulo/_form.php
@@ -16,29 +16,31 @@ use yii\web\View;
 /* @var $model app\models\Detaarticulo */
 /* @var $form yii\widgets\ActiveForm */
 $this->registerCssFile('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css');
-$this->registerJsFile('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js',['depends' => [JqueryAsset::class]]);
-$this->registerJsFile("/js/detaarticulo.js",['depends' => [JqueryAsset::class]]);
+$this->registerJsFile('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', ['depends' => [JqueryAsset::class]]);
+$this->registerJsFile("/js/detaarticulo.js", ['depends' => [JqueryAsset::class]]);
 $this->registerCss('
 .select2-selection{
     min-width:250px;
 ');
-if(empty($model->idsubcategoria)){
-    $id=0;
-}else{
-    $id=$model->idsubcategoria;
+if (empty($model->idsubcategoria)) {
+    $id = 0;
+} else {
+    $id = $model->idsubcategoria;
 }
-$sub = Subcategoria::find()->where(['idsubcategoria'=>$id])->one();
-$this->registerJs("
-var data = [
-    {
-        id: ".$id.",
-        text: '".$sub->nombre."'
-    }
-];
-$('.data-subcategoria').select2({
-    data:data
-  });
-");
+if ($id > 0) {
+    $sub = Subcategoria::find()->where(['idsubcategoria' => $id])->one();
+    $this->registerJs("
+    var data = [
+        {
+            id: " . $id . ",
+            text: '" . $sub->nombre . "'
+        }
+        ];
+        $('.data-subcategoria').select2({
+            data:data
+          });
+    ");
+}
 ?>
 
 <div class="detaarticulo-form">
@@ -47,23 +49,24 @@ $('.data-subcategoria').select2({
 
     <div class="row">
         <div class="col-md-6">
-        <label for="idsubcategoria">
-        Seleccione una Categoria
-        <select class="data-categoria custom-select" id="idcategoria" style="min-width: 600px;"></select>
-        </label>
+            <label for="idsubcategoria">
+                Seleccione una Categoria
+                <select class="data-categoria custom-select" id="idcategoria" style="min-width: 600px;"></select>
+            </label>
         </div>
     </div>
     <div class="row">
         <div class="col-md-6">
-        <label for="idsubcategoria">
-        Seleccione Subcategoria
-        <select name="Detaarticulo[idsubcategoria]" class="data-subcategoria custom-select" id="idsubcategoria" style="min-width: 600px;"></select>
-        </label>
+            <label for="idsubcategoria">
+                Seleccione Subcategoria
+                <select name="Detaarticulo[idsubcategoria]" class="data-subcategoria custom-select" id="idsubcategoria"
+                        style="min-width: 600px;"></select>
+            </label>
         </div>
     </div>
 
     <?php
-    $articulos= ArrayHelper::map(Articulo::find()->all(), 'idarticulo', 'descripcion');    
+    $articulos = ArrayHelper::map(Articulo::find()->all(), 'idarticulo', 'descripcion');
     echo $form->field($model, 'idarticulo')->widget(Select2::class, [
         'data' => $articulos,
         'options' => ['placeholder' => 'Seleccione un articulo'],
